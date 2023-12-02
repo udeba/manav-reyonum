590995861fda2410ccbc960602b3fcf2ec3fe8f5
udeba <143026150+udeba@users.noreply.github.com>
2023-12-02 10:21:03 +0100

    sepetin gorunumu duzeltildi

--- store.js
+++ store.js
@@ -12,7 +12,24 @@ function selectproduct(event) {
 }
 function addToCardItem(title, price, image) {
   let cardrow = document.createElement("div");
+  cardrow.classList.add("cart-row");
   cardrow.innerText = title;
   let carditemsway = document.querySelectorAll(".cart-items")[0];
+
+  let cardrowcontent = `<div class="cart-item cart-column">
+  <img
+    class="cart-item-image"
+    src="${image}"
+    width="100"
+    height="100"
+  />
+  <span class="cart-item-title">${title}</span>
+</div>
+<span class="cart-price cart-column">${price}</span>
+<div class="cart-quantity cart-column">
+  <input class="cart-quantity-input" type="number" value="2" />
+  <button class="btn btn-danger" type="button">REMOVE</button>
+</div>`;
+  cardrow.innerHTML = cardrowcontent;
   carditemsway.appendChild(cardrow);
 }
